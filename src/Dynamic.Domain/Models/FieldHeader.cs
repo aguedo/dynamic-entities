1abaf7136178cebf45bacfa22fcae6d990c5f8ca
Aguedo Valdes <aguedo@aslanta.com>
2025-04-17 10:54:45 -0500

    Remove the Type from the header.

--- src/Dynamic.Domain/Models/FieldHeader.cs
+++ src/Dynamic.Domain/Models/FieldHeader.cs
@@ -5,6 +5,5 @@ public class FieldHeader
     public string? Id { get; set; }
     public string? Name { get; set; }
     public string? DisplayName { get; set; }
-    public FieldType? Type { get; set; }
     public EntityType? EntityType { get; set; }
 }
