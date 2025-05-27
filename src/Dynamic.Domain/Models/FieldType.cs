9c53df4b17b8e02f312ae845147a97a0d9c28293
Ismel <diomedes.martnezdaz2@gmail.com>
2025-05-27 13:16:33 +0200

    refactor: simplify FieldType by removing enum and using constants

--- src/Dynamic.Domain/Models/FieldType.cs
+++ src/Dynamic.Domain/Models/FieldType.cs
@@ -1,43 +1,11 @@
 namespace Dynamic.Domain.Models;
 
-using System.ComponentModel.DataAnnotations;
-
-public enum FieldType
+public static class FieldType
 {
-    [Display(Name = "string")]
-    String,
-    [Display(Name = "integer")]
-    Integer,
-    [Display(Name = "boolean")]
-    Boolean,
-    [Display(Name = "datetime")]
-    DateTime,
-    [Display(Name = "decimal")]
-    Decimal,
-    [Display(Name = "guid")]
-    Guid
-}
-
-public static class FieldTypeExtensions
-{
-    private static Dictionary<FieldType, string> _fieldTypeDisplayNamesCache = new();
-    public static string GetDisplayName(this FieldType fieldType)
-    {
-        try
-        {
-            return _fieldTypeDisplayNamesCache[fieldType];
-        }
-        catch (KeyNotFoundException)
-        {
-            var displayAttribute = fieldType.GetType()
-            .GetField(fieldType.ToString())
-            ?.GetCustomAttributes(typeof(DisplayAttribute), false)
-            .FirstOrDefault() as DisplayAttribute;
-
-            var name = displayAttribute?.Name ?? fieldType.ToString();
-            _fieldTypeDisplayNamesCache.Add(fieldType, name);
-            return name;
-        }
-
-    }
+    public const string Integer = "Integer";
+    public const string String = "String";
+    public const string DateTime = "DateTime";
+    public const string Boolean = "Boolean";
+    public const string Decimal = "Decimal";
+    public const string Guid = "Guid";
 }
